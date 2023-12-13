c43eec7b6fbf2b6ea628ff21a928d5c470789f69
Aaron Peterham <47309189+amp7368@users.noreply.github.com>
2023-12-13 17:28:48 -0500

    Remove log4j dependency. Use Consumer for more compatibility

--- src/main/java/apple/lib/ebean/database/AppleEbeanDatabaseMetaConfig.java
+++ src/main/java/apple/lib/ebean/database/AppleEbeanDatabaseMetaConfig.java
@@ -1,33 +1,32 @@
 package apple.lib.ebean.database;
 
 import java.io.File;
-import org.apache.logging.log4j.Logger;
+import java.util.function.Consumer;
 
 public class AppleEbeanDatabaseMetaConfig {
 
     private static Class<?> mainClass;
-    private static Logger logger;
     private static File rootFolder;
+    private static Consumer<String> errorLogger;
+    private static Consumer<String> infoLogger;
 
     public static void logInfo(String msg) {
-        if (logger == null) System.out.println(msg);
-        else logger.info(msg);
+        if (infoLogger == null) System.out.println(msg);
+        else infoLogger.accept(msg);
     }
 
     private static void logError(String msg) {
-        if (logger == null) System.err.println(msg);
-        else logger.error(msg);
+        if (errorLogger == null) System.err.println(msg);
+        else errorLogger.accept(msg);
     }
 
-    public static void configureMeta(Class<?> mainClass, File rootFolder, Logger logger) {
+    public static void configureMeta(Class<?> mainClass, File rootFolder, Consumer<String> errorLogger, Consumer<String> infoLogger) {
         setMainClass(mainClass);
         setRootFolder(rootFolder);
-        setLogger(logger);
+        AppleEbeanDatabaseMetaConfig.errorLogger = errorLogger;
+        AppleEbeanDatabaseMetaConfig.infoLogger = infoLogger;
     }
 
-    public static void setLogger(Logger logger) {
-        AppleEbeanDatabaseMetaConfig.logger = logger;
-    }
 
     public static Class<?> getMainClass() {
         if (mainClass == null) logError("'mainClass' not set in " + className());
